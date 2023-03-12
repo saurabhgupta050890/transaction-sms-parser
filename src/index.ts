1bdc04462847bf5475be5f0215edf7361a3d52ae
Saurabh Gupta <saurabhgupta0508@gmail.com>
2023-03-13 01:16:48 +0530

    chore: export types

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import getAccount from './lib/account';
 import getBalance from './lib/balance';
 
 export * from './lib/engine';
+export * from './lib/interface';
 export const getAccountInfo = getAccount;
 export const getBalanceInfo = getBalance;
