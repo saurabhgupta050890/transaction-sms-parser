e7f34e1c832c6f9957e7cfa09496a483433e5c99
saurabhgupta050890 <saurabhgupta050890@users.noreply.github.com>
2026-05-27 23:18:28 +0530

    fix: add missing 'ref' keyword to upiKeywords array

--- src/library/constants.ts
+++ src/library/constants.ts
@@ -23,7 +23,7 @@ export const outstandingBalanceKeywords = ["outstanding"];
 
 export const wallets = ["paytm", "simpl", "lazypay", "amazon_pay"];
 
-export const upiKeywords = ["upi", "ref no", "upi ref", "upi ref no"];
+export const upiKeywords = ["upi", "ref no", "upi ref", "upi ref no", "ref"];
 
 export const combinedWords: ICombinedWords[] = [
 	{
